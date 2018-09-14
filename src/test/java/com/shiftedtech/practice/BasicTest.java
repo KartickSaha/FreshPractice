a881b0e8ef377876e7940e8e652e09f9ffa5b821
Kartick <kartick01@yahoo.com>
2018-09-14 18:25:01 -0400

    test of github

---
+++ src/test/java/com/shiftedtech/practice/BasicTest.java
@@ -0,0 +1,21 @@
+package com.shiftedtech.practice;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasicTest {
+    @Before
+    public void setUP(){
+
+    }
+ @Test
+    public void test01(){
+     System.out.println("Hello");
+ }
+
+ @After
+    public void tearDown(){
+
+ }
+}
